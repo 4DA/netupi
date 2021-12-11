39d812ba9bb0a2e914a0f65996609372f250067b
4DA <4da@sandycat.info>
2021-12-11 20:27:55 +0300

    [ui] duration_widget: align duration to right

--- src/widgets.rs
+++ src/widgets.rs
@@ -9,19 +9,19 @@ pub fn duration_widget() -> impl Widget<Rc<time::AggregateDuration>> {
     let label = Label::new(|duration: &Rc<time::AggregateDuration>, _env: &_| {
         let mut result = String::new();
 
-        result.push_str(&time::format_duration(&duration.day));
+        result.push_str(&format!("{:>12}", time::format_duration(&duration.day)));
         result.push_str("\n");
 
-        result.push_str(&time::format_duration(&duration.week));
+        result.push_str(&format!("{:>12}", time::format_duration(&duration.week)));
         result.push_str("\n");
 
-        result.push_str(&time::format_duration(&duration.month));
+        result.push_str(&format!("{:>12}", time::format_duration(&duration.month)));
         result.push_str("\n");
 
-        result.push_str(&time::format_duration(&duration.year));
+        result.push_str(&format!("{:>12}", time::format_duration(&duration.year)));
         result.push_str("\n");
 
-        result.push_str(&time::format_duration(&duration.total));
+        result.push_str(&format!("{:>12}", time::format_duration(&duration.total)));
 
         return result;
     }).with_font(FONT_LOG_DESCR.clone());
